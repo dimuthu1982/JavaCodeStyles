f14644673ba6f1283b89474efff19ee06b71aed5
Dimuthu Abeynayake <dimuthuabeynayake@10.0.0.113>
2016-07-21 15:54:09 +1000

    Posting callers method to callee

--- src/my/test/samples/SampleJavaProject.java
+++ src/my/test/samples/SampleJavaProject.java
@@ -7,7 +7,7 @@ public class SampleJavaProject {
 
   public static void main(String[] args) {
 
-//     ------------------- Accessing HashMap -------------------
+//     =================== Accessing HashMap  ===================
     Map<String,String> dataMap = new HashMap<>();
 
 //    Old Way
@@ -20,6 +20,28 @@ public class SampleJavaProject {
       System.out.println(element.getKey() +" --> " + element.getValue());
     }
 //    Note: The new way is much faster in accessing a map
+    
+//  =================== Posting callers method to callee  ===================
+    IMethodHolder privateMethodHolder = new IMethodHolder() {
+      
+      @Override
+      public void privateMethodWrapper() {
+        new SampleJavaProject().printingMessage();
+        
+      }
+    };
+    
+    PrivateMethodUser privateMethodUser = new PrivateMethodUser(privateMethodHolder);
+    privateMethodUser.privateMethodPrinter();
+    /*
+    Note : When an class needs to use a method (probably for re-usability or cyclic dependency elimination) an interface can be used to envelop a method and
+    post to a class as a parameter. On this occasion PrivateMethodUser will pass the private method printingMessage() a a parameter wrap in a interface IMethodHolder.
+    Hence enabling the PrivateMethodUser to obtain access to the callers method by triggering a call privateMethodPrinter();
+    */
+  }
+  
+  private void printingMessage(){
+    System.out.println("Hello From Main Class");
   }
 
 }
