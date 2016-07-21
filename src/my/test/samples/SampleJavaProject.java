a80b4d28a3f634afc97a24ed1003eb2eb2a1af48
Dimuthu Abeynayake <dimuthuabeynayake@10.0.0.113>
2016-07-21 15:39:10 +1000

    Accessing HashMap with Map.Entry

--- src/my/test/samples/SampleJavaProject.java
+++ src/my/test/samples/SampleJavaProject.java
@@ -1,10 +1,25 @@
 package my.test.samples;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SampleJavaProject {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+//     ------------------- Accessing HashMap -------------------
+    Map<String,String> dataMap = new HashMap<>();
+
+//    Old Way
+    for (String key : dataMap.keySet()) {
+      System.out.println(key +" --> " + dataMap.get(key));
+    }
+    
+//    New Way
+    for (Map.Entry<String,String> element : dataMap.entrySet()) {
+      System.out.println(element.getKey() +" --> " + element.getValue());
+    }
+//    Note: The new way is much faster in accessing a map
   }
 
 }
